757c0c66b583207afc637347303c759d7479057b
kuba <kuba.grudzien1999.jg@gmail.com>
2018-07-13 12:54:31 +0200

    Tomku, załączam Monte Casino Algorithm z komentarzami. Pozdrowienia dla rodziny!

--- monte.py
+++ monte.py
@@ -10,7 +10,7 @@ proba = np.array([[[[[0]*m]*n]*m]*n]*4)
 dir_r = [-1, 0, 1, 0]
 dir_c = [0, 1, 0, -1]
 counter = np.array([[[0.]*m]*n]*4)
-sum = np.array([[[0.]*m]*n]*4)
+sum = np.array([[[0.]*m]*n]*4) #suma rewardow
 q = np.array([[[0.]*m]*n]*4)
 ile = [[0]*4]*3
 
@@ -18,6 +18,7 @@ ile = [[0]*4]*3
 def simu(i, j, pi):
     trace = []
     while j > 0:
+        #losowanie wyboru akcji
         number = random.uniform(0, 1)
         act = 0
         action = 0
@@ -27,15 +28,14 @@ def simu(i, j, pi):
                 break
             else:
                 act += pi[a][i][j]
+
+        #losowanie rzeczywistej akcji
         number = random.uniform(0, 1)
         act = 0
         for r in range(n):
             for c in range(m):
                 if proba[action][i][j][r][c] + act >= number:
                     trace.append((i, j, action))
-                    ile[0][i] += 1
-                    ile[0][j] += 1
-                    ile[0][action] += 1
                     i = r
                     j = c
                 else:
@@ -46,6 +46,7 @@ def simu(i, j, pi):
         output = 0
     return (trace, output)
 
+#ustalamy prawdopodobienstwa dla skrajnych klockow
 for r in range(n):
     for c in range(m):
         for a in range(4):
@@ -54,6 +55,7 @@ for r in range(n):
             else:
                 proba[a][r][c][r][c] = 1
 
+#ustalamy prawdopodobienstwa dla lewej krawedzi
 for r in range(n):
     for a in range(4):
         for i in range(n):
@@ -61,12 +63,14 @@ for r in range(n):
                 proba[a][r][0][i][j] = 0
         proba[a][r][c][r][c] = 1
 
+#liczymy srednie dla pol
 for i in range(1000):
     x, out = simu(3, 3, pi)
     for j in range(len(x)):
         counter[x[j][2]][x[j][0]][x[j][1]] += 1
         sum[x[j][2]][x[j][0]][x[j][1]] += out
 
+#ustalamy wartosci q
 for i in range(n):
     for j in range(m):
         for a in range(4):
